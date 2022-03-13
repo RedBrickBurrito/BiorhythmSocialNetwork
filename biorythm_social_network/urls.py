3fcce868f2ec2fcf3c6f6179fec593ddf80eae79
Javinator11 <sosa.puga.javier@gmail.com>
2022-03-12 22:20:47 -0700

    Added Logout Functionality

--- biorythm_social_network/urls.py
+++ biorythm_social_network/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from biorhythm.views import HomePage
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', HomePage, name='home'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('admin/', admin.site.urls),
 ]
