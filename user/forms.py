8ab2af099a905e8a178d754224081f06afc7af83
Eddy Almanza <a01568010@gmail.com>
2022-03-17 19:08:34 -0700

    BSN-1 Added Design

--- user/forms.py
+++ user/forms.py
@@ -1,16 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.db import models
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(max_length=200)
+    email = forms.EmailField(max_length=200, required=True)
     birthday = forms.DateField(
         widget=forms.SelectDateWidget(
             years=range(1980, 2022)
-        )
+        ),         required=True
     )
-    image = forms.ImageField()
+    image = forms.ImageField(required=True)
 
     class Meta:
         model = CustomUser
