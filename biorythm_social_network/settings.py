7bdae999ab02f5cdd1b2d6f2b5d788ef66e6ec08
Eddy Almanza <a01568010@gmail.com>
2022-03-23 00:46:17 -0700

    Added Gunicorn

--- biorythm_social_network/settings.py
+++ biorythm_social_network/settings.py
@@ -137,7 +137,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
+TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
